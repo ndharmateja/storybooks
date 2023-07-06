f038630e4c029acdab4908cd82ed05e5fd23d9e3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-06 18:14:41 +0530

    Add static folder to express app

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import { unknownRoute } from "./utils/middleware.js";
 import morgan from "morgan";
 import { engine } from "express-handlebars";
 import { router } from "./routes/index.js";
+import * as path from "path";
+import { fileURLToPath } from "url";
 
 // Connect to DB
 await connectDb();
@@ -18,6 +20,12 @@ if (isDev) app.use(morgan("dev"));
 app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
+
+// Static assets
+// Reference: https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", router);
