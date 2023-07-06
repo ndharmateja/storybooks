3a02b984528eca721aa29cfdab1648388304134e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-06 17:10:51 +0530

    Call connectDb in app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { NODE_ENV, PORT } from "./utils/config.js";
+import { connectDb } from "./utils/db.js";
 
 // Express app
 const app = express();
+
+// Connect to DB
+await connectDb();
 
 // Listen
 app.listen(PORT, () =>
