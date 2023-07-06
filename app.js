690ae6dca0a9be3de61e681c12119a6741c21a67
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-06 17:42:11 +0530

    Add mw to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { NODE_ENV, PORT } from "./utils/config.js";
+import { isDev, PORT } from "./utils/config.js";
 import { connectDb } from "./utils/db.js";
+import { unknownRoute } from "./utils/middleware.js";
+import morgan from "morgan";
 
 // Connect to DB
 await connectDb();
@@ -8,7 +10,14 @@ await connectDb();
 // Express app
 const app = express();
 
+// Middleware
+app.use(express.json());
+if (isDev) app.use(morgan("dev"));
+
+// Routes
+app.use(unknownRoute);
+
 // Listen
 app.listen(PORT, () =>
-  console.log(`Server running in '${NODE_ENV}' on: ${PORT}`)
+  console.log(`[${isDev ? "dev" : "prod"}] Server running in on: ${PORT}`)
 );
