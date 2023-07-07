209b3a92e5f4017bbc3d1879c935e4f9f78086e5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-07 09:55:45 +0530

    Add google client id and secret to config

--- utils/config.js
+++ utils/config.js
@@ -5,9 +5,18 @@ dotenv.config({ path: "./config/config.env" });
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 const NODE_ENV = process.env.NODE_ENV;
 const isProd = NODE_ENV === "production";
 const isDev = NODE_ENV === "development";
 
-export default { PORT, MONGODB_URI, isDev, isProd };
+export default {
+  PORT,
+  MONGODB_URI,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  isDev,
+  isProd,
+};
